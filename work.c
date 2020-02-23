4108a20ae02034bcf44fd7e42bbb1aab3c1c4319
root <nskwak@hotmail.com>
2020-02-22 23:39:55 -0800

    20200222:RU

--- work.c
+++ work.c
@@ -1,32 +1,413 @@
-#if 1   //KK_ 
+#if 1   
 ///////////////////////////////////////////////////////////////////////////////////
-// function: binary search 
+// function: bit maipulation
+// Cracking code page 57, 5.5
+// input: 31, 14 -> output: 2
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+#define bit0    0
+#define bit1    1
+#define bit2    2
+#define bit3    3
+#define bit4    4
+#define bit5    5
+#define bit6    6
+#define bit7    7
+
+int countXorValue(int in_num)
+{
+    int count=0;
+    while(in_num)
+    {
+        count += (in_num & 1);
+        in_num = (in_num >> 1);
+    }
+    return count;
+}
+
+int swapbit(int in_num)
+{
+    return ((0xaaaaaaaa & in_num)>>1 | (0x55555555 & in_num)<<1);
+}
+
+int main()
+{
+    int a = 31;
+    int b = 14;
+    int flag = 0x00;
+
+    flag = flag | (1<<bit1) | (1<<bit3) | (1<<bit5) | (1<<bit7);
+    printf("flag = 0x%x , 0x%x\n", flag, flag&0x2);
+    printf("0x%x\n", ~0xff);
+
+    if(flag && 0x3)
+    {
+        printf("flag = 0x%x \n", flag);
+    }
+
+    printf("count: %d \n", countXorValue(a^b));
+
+    int c = 0x11111111;
+    printf("swapbit of 0x%x = 0x%x\n", c, swapbit(c));
+
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////
+#endif
+
+#if 0   
+///////////////////////////////////////////////////////////////////////////////////
+// function: bit maipulation
+// Cracking code page 58, 5.5
+// input: 31, 14 -> output: 2
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+int countXorValue(int in_num)
+{
+    int count=0;
+    while(in_num)
+    {
+        count += (in_num & 1);
+        in_num = (in_num >> 1);
+    }
+    return count;
+}
+
+int main()
+{
+    int a = 31;
+    int b = 14;
+
+    printf("count: %d \n", countXorValue(a^b));
+
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////
+#endif
+
+#if 0   
+///////////////////////////////////////////////////////////////////////////////////
+// function: bit maipulation
+// Cracking code page 57, 5.1
+// a)  N = 1024 (10000000000),
+//     M = 19 (10011),
+//     i = 2, j = 6
+//     Output : 1100 (1000'10011'00)
+// b)  N = 1201 (10010110001)
+//     M = 8 (1000)
+//     i = 3, j = 6
+//     Output: 1217 (1001'1000'001)
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+void bin(int in_num)
+{
+    if(in_num > 1)
+        bin(in_num/2);
+    printf("%d ", in_num%2);
+}
+
+int main()
+{
+    int N = 1201;
+    int M = 8;
+    int i = 3;
+    int j = 6;
+    int captured_mask, clear_mask;
+
+    printf("N = %d -> ", N);
+    bin(N); printf("\n");
+    captured_mask = (1 << i) - 1;
+    captured_mask &= N;
+    printf("captured_mask = %d \n", captured_mask);
+    clear_mask = -1 << (j+1);
+    printf("clear_mask = %d \n", clear_mask);
+    ///////////////////////////////////////////////////////////
+    N &= clear_mask;
+    printf("%d, ", N); bin(N); printf("\n");
+
+    N |= (M<<i);
+    printf("%d, ", N); bin(N); printf("\n");
+
+    N |= captured_mask;
+    printf("%d, ", N); bin(N); printf("\n");
+
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////
+#endif
+
+#if 0   
+///////////////////////////////////////////////////////////////////////////////////
+// function: Linked list Restudy #2
+// create node, add new data
+//
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+typedef struct Node 
+{
+    int data;
+    struct Node* next;
+}NODE;
+
+// insert data at the beginning
+void insert(NODE** head_ref, int datain)
+{
+    NODE* temp = malloc(sizeof(NODE));
+    temp->data = datain;
+    temp->next = *head_ref;
+    *head_ref = temp;
+    //printf("insert: addr = %p\n", temp);
+}
+
+// append data at the end
+void append(NODE** head_ref, int datain)
+{
+    NODE* temp = malloc(sizeof(NODE));
+    NODE* cur = *head_ref;
+
+    temp->data = datain;
+    temp->next = NULL;
+
+    while(*head_ref == NULL)
+    {
+        *head_ref = temp;
+        return;
+    }
+    while(cur->next != NULL)
+    {
+        cur = cur->next;
+    }
+    cur->next = temp;
+    //printf("append: addr = %p\n", temp);
+}
+
+// insert after nthnode
+void insertafternode(NODE** head_ref, int nth, int datain)
+{
+    NODE* temp = malloc(sizeof(NODE));
+    NODE* cur = *head_ref;
+    NODE* prev;
+    int i = 0;
+
+    while((cur != NULL) && (i != nth))
+    {
+        prev = cur;
+        cur = cur->next;
+        i++;
+    }
+    if(cur==NULL)
+    {
+        printf("NO data can be inserted!!\n");
+        return;
+    }
+    temp->data = datain;
+    temp->next = cur;
+    prev->next = temp;
+    //*head_ref = temp;
+    //printf("insert: addr = %p\n", temp);
+}
+
+// delete nthnode
+void deleteNthNodeFromEnd(NODE** head_ref, int nth)
+{
+    NODE* temp = malloc(sizeof(NODE));
+    NODE* cur = *head_ref;
+    NODE* prev;
+    int i = 0;
+
+    while(cur != NULL)
+    {
+        cur = cur->next;
+        i++;
+    }
+    if(nth >= i)
+    {
+        printf("NO data can be deleted!!\n");
+        return;
+    }
+    cur = *head_ref;
+    printf("i=%d, nth=%d\n", i, nth);
+    //i = i - nth;
+    while(i > nth)
+    {
+        printf("i=%d\n", i);
+        prev = cur;
+        cur = cur->next;
+        i--;
+    }
+    prev->next = cur->next;
+    free(cur);
+}
+
+void delete(NODE** head_ref, int deletedata)
+{
+    NODE* cur = *head_ref;
+    NODE* prev;
+    
+    while((cur != NULL) && (cur->data != deletedata))
+    {
+        prev = cur;
+        cur = cur->next;
+    }
+    if(cur->next == NULL)
+    {
+        printf("NO data can be deleted!!\n");
+        return;
+    }
+    prev->next = cur->next;
+    free(cur);
+}
+
+void printlist(NODE* cur)
+{
+    while(cur != NULL)
+    {
+        printf("%d ", cur->data);
+        cur = cur->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    NODE* head = NULL;
+    insert(&head, 1);
+    append(&head, 2);
+    append(&head, 3);
+    append(&head, 4);
+    printf("display list added              : "); printlist(head);
+    delete(&head, 2);
+    printf("delete '2'                      : "); printlist(head);
+    append(&head, 5);
+    append(&head, 6);
+    printf("add 5, 6                        : "); printlist(head);
+    insertafternode(&head, 4, 99);
+    printf("insert 99 after 4th node        : "); printlist(head);
+    deleteNthNodeFromEnd(&head, 4);
+    printf("delete 4th node from the end    : "); printlist(head);
+}
+#endif
+
+#if 0   
+///////////////////////////////////////////////////////////////////////////////////
+// function: Linked list Restudy #1
+// create node, add new data
+//
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+typedef struct Node 
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}NODE;
+
+NODE *newnode(int datain)
+{
+    NODE* temp = malloc(sizeof(NODE));
+    temp->data = datain;
+    temp->left = temp->right = NULL;
+    return(temp);
+}
+
+void printlist(NODE* cur)
+{
+    if(cur == NULL)
+        return;
+    printf("%d ", cur->data);
+    printlist(cur->left);
+    printlist(cur->right);
+}
+
+int main()
+{
+    NODE* root = NULL;
+    root = newnode(1);
+    root->left = newnode(2);
+    root->right = newnode(3);
+    root->right->left = newnode(4);
+    root->right->right = newnode(5);
+    printlist(root);
+}
+#endif
+
+
+
+#if 0   //KK_ unsolved yet,  need to solved.
+///////////////////////////////////////////////////////////////////////////////////
+// function: LeetCode:Easy 1. Two Sum with hash table
+// Input:  [2, 3, 7, 11, 13], target=9
+// Output: [0, 2]
 #include <stdio.h>
 #include <malloc.h>
 #include <unistd.h>
 
 int main()
 {
-    int sorted_number[] = {1, 4, 6, 7, 8, 9};
+    int in_num[] = {2, 3, 7, 11, 13};
     int target = 9;
-    int loopcnt = sizeof(sorted_number)/sizeof(char);
+    int i, k;
+    int loopcnt = sizeof(in_num)/sizeof(int);
+    int hashTarr[256] = {};
+
     printf("%d \n", loopcnt);
 
-    for(i=0; i<(loopcnt/2); i++)
+    for(int i=0; i<loopcnt; i++)
     {
-        k = str[i];
-        str[i] = str[loopcnt-1-i];
-        str[loopcnt - 1 - i] = k;
-        printf("%d: str[%d]:%c  str[%d]:%c\n", i, i, str[i], loopcnt - 1 - i, str[loopcnt - 1 - i]);
-    }
+        for(int k=1; k<loopcnt; k++)
+        {
 
-    //check result
-    for(i=0; i<loopcnt; i++)
-    {
-        printf("%c", str[i]);
+            if((in_num[i] + in_num[k]) == target)
+            {
+                printf("%d + %d = %d\n", in_num[i], in_num[k], target);
+                break;
+            }
+        }
+        hashTarr[i] = target - in_num[i];
     }
 }
 #endif
+
+#if 0   //KK_ not start yet
+///////////////////////////////////////////////////////////////////////////////////
+// function: just double check pointer
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+int main()
+{
+    int i;
+    int *p;
+    char a, b;
+
+    i = 3;
+    *p = 4;
+    a = 'a';
+    b = 'b';
+
+    printf("i = %d\n", i);  // print i value
+    printf("i = %p\n", &i); // print i address
+    printf("p = %d\n", *p); // print p value
+    printf("p = %p\n", &p); // print p address
+    printf("a = %c\n", a);  // print a value
+    printf("a = %p\n", &a);  // print a address
+    printf("b valu = %c\n", b);  // print a value
+    printf("b addr = %p\n", &b);  // print a address
+
+}
+#endif
+
 
 
 #if 0   //KK_ 
@@ -97,7 +478,106 @@ int main()
 
 #if 0   //KK_ 
 ///////////////////////////////////////////////////////////////////////////////////
-// function: remove duplicate with hash table
+// function: find first duplicate with hash table
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+int main()
+{
+    int str[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'd'};
+    int i;
+    int arr[256] = {};
+
+    for(i=0; str[i]; i++)
+    {
+        arr[str[i]]++;
+        if(arr[str[i]] == 2)
+        {
+            printf("%c\n", str[i]);
+            break;
+        }
+    }
+}
+#endif
+
+
+#if 0   //KK_ this is too hard, will study later
+///////////////////////////////////////////////////////////////////////////////////
+// function: 316. Remove Duplicate Letters by using hash table
+// Input: "bcabc" -> Output: "abc"
+// Input: "cbacdcbc" -> Output: "acdb"
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+int main()
+{
+    char str[] = {"bcabc"};
+    int i;
+    char arr[256] = {};
+    char result[256] = {};
+    int k=0;
+
+    for(i=0; str[i]; i++)
+    {
+        arr[str[i]]++;
+        if(arr[str[i]] == 1)
+        {
+            printf("%c\n", str[i]);
+            result[k++] = str[i];
+        }
+    }
+
+    //print output
+    printf("input  = %s\noutput = ", str);
+    for(i=0;i<k;i++)
+    {
+        printf("%c", result[i]);
+    }
+    printf("\n");
+}
+#endif
+
+
+#if 0   //KK_ resolved, this is not very hard.
+///////////////////////////////////////////////////////////////////////////////////
+// function: 316. Remove Duplicate Letters by using hash table for string input
+// Input: "bcabc" -> Output: "bca"
+// Input: "cbacdcbc" -> Output: "cbad"
+#include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+
+int main()
+{
+    char str[] = {"bcabc"};
+    int i;
+    char arr[256] = {};
+    char result[256] = {};
+    int k=0;
+
+    for(i=0; str[i]; i++)
+    {
+        arr[str[i]]++;
+        if(arr[str[i]] == 1)
+        {
+            printf("%c\n", str[i]);
+            result[k++] = str[i];
+        }
+    }
+    printf("input  = %s\noutput = ", str);
+    for(i=0;i<k;i++)
+    {
+        printf("%c", result[i]);
+    }
+    printf("\n");
+}
+#endif
+
+#if 0   //KK_ 
+///////////////////////////////////////////////////////////////////////////////////
+// function: delete duplicate with hash table
 #include <stdio.h>
 #include <malloc.h>
 #include <unistd.h>
@@ -127,32 +607,19 @@ int main()
 }
 #endif
 
-
-#if 0   //KK_ 
-///////////////////////////////////////////////////////////////////////////////////
-// function: find first duplicate with hash table
-#include <stdio.h>
-#include <malloc.h>
-#include <unistd.h>
-
-int main()
-{
-    int str[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'd'};
-    int i;
-    int arr[256] = {};
-
-    for(i=0; str[i]; i++)
-    {
-        arr[str[i]]++;
-        if(arr[str[i]] == 2)
-        {
-            printf("%c\n", str[i]);
-            break;
-        }
-    }
-}
-#endif
-
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+// after meeting with SM Kang, 2/15/2020
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+///////////////////////////////////////////////
 
 
 #if 0   //KK_ 
@@ -208,8 +675,6 @@ int main()
     printf("fibonacci of %d is %d\n", num, result);
 }
 #endif
-
-
 
 
 #if 0   //KK_ 
@@ -481,7 +946,7 @@ int main()
 
 #if 0    
 ///////////////////////////////////////////////////////////////////////////////////
-// function: linkedlist typedef
+// function: linkedlist ***************** typedef ******************
 #include <stdio.h>
 #include <malloc.h>
 
@@ -595,12 +1060,16 @@ void delete(struct Node** head_ref, int delete_data)
         prev = cur;
         cur = cur->pointer; 
     }
+    printf("A. cur data = %d, cur node: %p, next:%p, -> prev data=%d, %p, next:%p\n", cur->data, cur, cur->pointer, prev->data, prev, prev->pointer);
     //if cur=NULL, there is no matched data to delete
     if (cur == NULL)
+    {
         printf("there is no %d to delete!!!!!\n", delete_data);
         return;
+    }
     //copy curr->pointer to pre->pointer and delete cur node.
     prev->pointer = cur->pointer;
+    printf("B. cur data = %d, cur node: %p, next:%p, -> prev data=%d, %p, next:%p\n", cur->data, cur, cur->pointer, prev->data, prev, prev->pointer);
     free(cur);
     return;
 }
@@ -645,16 +1114,17 @@ int main()
     append(&head, 33);
     append(&head, 44);
     insert(&head, 2);
+    printf("1. ");     printlist(head);
     insertAfter(head, 100);
-    printlist(head);
+    printf("2. ");     printlist(head);
     delete(&head, 100);
-    printlist(head);
+    printf("3. ");     printlist(head);
     delete(&head, 2);
-    printlist(head);
+    printf("4. ");     printlist(head);
     delete(&head, 44);
-    printlist(head);
+    printf("5. ");     printlist(head);
     delete(&head, 4);
-    printlist(head);
+    printf("6. ");     printlist(head);
     update(&head, 33, 22);
     printlist(head);
 
@@ -669,6 +1139,7 @@ int main()
 // 2020/02/11 
 // function: linked list #1
 // https://www.geeksforgeeks.org/linked-list-set-2-inserting-a-node/
+// insert(at beginning), append(at the end), insertafter(insert after node), printlist
 
 // A complete working C program to demonstrate all insertion methods 
 // on Linked List 
