8bc02231bb9493e682fdc3fbeb3d48a188e536d7
root <nskwak@hotmail.com>
2020-03-08 10:15:29 -0700

    20200308_1

--- 72_SMKInterFindUncopiedArray.c
+++ 72_SMKInterFindUncopiedArray.c
@@ -14,9 +14,26 @@ int main()
     int arrN[] = {1, 3, 4, 6, 7};
     int copiedArrayN[] = {1, 3, 4, 7};
     int lenGth = sizeof(arrN)/sizeof(int);
-    int found = 0;
     int loopcnt = 0;
 
+    #if 1
+    for(int i=0; i<lenGth; i++)    
+    {
+        loopcnt++;
+        if(arrN[i] == copiedArrayN[i])
+        {
+            printf("%d   %d\n", arrN[i], copiedArrayN[i]);
+            continue;
+        }
+        else
+        {
+            printf("%d   %d\n", arrN[i], copiedArrayN[i]);
+            printf("uncopied arrayN is %d\n", arrN[i]);
+            break;
+        }
+    }
+    #else
+    int found = 0;
     for(int i=0; i<lenGth; i++)    
     {
         for(int j=0; j<(lenGth-1); j++)
@@ -36,7 +53,9 @@ int main()
         }
         
     }
+    #endif
     printf("Total loop count = %d\n", loopcnt);
+
     return 0;
 }
 #endif
